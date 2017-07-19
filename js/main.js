342e49f5f7677efbacef2c84c3636cd9f3453890
bfdykstra <bfdykstra@email.wm.edu>
2017-07-19 14:42:13 -0400

    still making game logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("user is out of their damn mind " + cardTwo);
-
-if (cardsInPlay.length === 2){
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
+function checkForMatch(){
+  if( cardsInPlay[0] === cardsInPlay[1]){
+    console.log("You found a match");
   }else{
-    alert("sorry, try again");
+    console.log("sorry, try again.");
   }
 }
+
+function flipCard(cardId) {
+  console.log("User flipped a " + cards[cardId]);
+
+  cardsInPlay.push(cards[cardId]);
+
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
